91bf255b21b5f53e2178b497429d533135c5dac2
Christabel Sebastian <christabel.escarez@gmail.com>
2020-12-27 20:03:52 -0800

    Wrote timeline component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+
+  return(
+    <div className='timeline'>
+      {props.events.map ((timelineEvent, i) => (
+          <TimelineEvent person={timelineEvent.person} status={timelineEvent.status} timestamp={timelineEvent.timestamp}/>
+      ))}
+    </div>
+  );
 }
 
 export default Timeline;
