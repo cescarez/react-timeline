5518c7556b2dfcf77041ead125e04e132efa8248
Christabel Sebastian <christabel.escarez@gmail.com>
2020-12-27 20:04:04 -0800

    implemented timeline rendering in app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import React from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
-import TimelineEvent from './components/TimelineEvent';
 
 function App() {
-  console.log(timelineData);
-
   // Customize the code below
   return (
     <div className="App">
@@ -15,7 +12,7 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
-        <TimelineEvent person='Michelle Obama' status='Hello, World!' timestamp='2018-05-18T22:12:03Z' />
+        <Timeline events={timelineData.events} />
       </main>
     </div>
   );
